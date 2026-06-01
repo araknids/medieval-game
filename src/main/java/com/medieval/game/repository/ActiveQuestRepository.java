98ce5d6c4066ffb499bf7435adeaf51cae441db7
araknids <rodrigo.horacio.contato@gmail.com>
2026-06-01 16:53:43 -0300

    fix: eager load warrior in quest repository to fix LazyInitializationException

--- src/main/java/com/medieval/game/repository/ActiveQuestRepository.java
+++ src/main/java/com/medieval/game/repository/ActiveQuestRepository.java
@@ -1,14 +1,20 @@
 package com.medieval.game.repository;
 
+import com.medieval.game.enums.QuestStatus;
 import com.medieval.game.model.ActiveQuest;
 import com.medieval.game.model.Player;
-import com.medieval.game.enums.QuestStatus;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ActiveQuestRepository extends JpaRepository<ActiveQuest, Long> {
+
+    @EntityGraph(attributePaths = {"warrior"})
     List<ActiveQuest> findAllByPlayerAndStatusNot(Player player, QuestStatus status);
-    List<ActiveQuest> findAllByPlayerAndStatusNotIn(Player player, java.util.List<QuestStatus> statuses);
+
+    @EntityGraph(attributePaths = {"warrior"})
+    List<ActiveQuest> findAllByPlayerAndStatusNotIn(Player player, List<QuestStatus> statuses);
+
     List<ActiveQuest> findAllByPlayer(Player player);
 }
